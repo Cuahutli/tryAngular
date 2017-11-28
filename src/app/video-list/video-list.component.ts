09f6dcfe1ca6e3ffd8564d1eb443b4bb7425fda0
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-11-28 14:36:50 -0700

    agregando variables a video-list (variable title)

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-list.component.css']
 })
 export class VideoListComponent implements OnInit {
-
+  title = 'Un nuevo componente'
   constructor() { }
 
   ngOnInit() {
