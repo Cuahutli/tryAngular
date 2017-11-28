df7385284f907ba5f0af2b2a8287a156fd03db64
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-11-28 14:24:46 -0700

    iniciando tryangular4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hola Srvup2';
+  description = 'Una nueva aplicación';
 }
