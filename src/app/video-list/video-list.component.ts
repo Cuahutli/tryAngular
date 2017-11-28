b08001909c3b1cc1ff951aed9528ff4d5b59f25c
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-11-28 14:43:09 -0700

    ejemplo de uso de ngfor y ngif (el ngif no lo hice)

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-list.component.css']
 })
 export class VideoListComponent implements OnInit {
-  title = 'Un nuevo componente'
+  title = 'Lista de Videos';
+  //videoList = ["Item 1", "Item 2", "Item 3"]; //JSON
+  videoList = [
+    {
+      name: "Item 1",
+      slug: "item-1"
+    },
+    {
+      name: "Item 1",
+      slug: "item-1"
+    },
+    {
+      name: "Item 1",
+      slug: "item-1"
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
