e67f04b8ca9da7384d6bb2de8973cbdf383dff63
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-12-01 10:41:48 -0700

    14 Angular Click Events

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'home',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  homeImageList =[
+    {
+      image: "assets/images/nature/4.jpg",
+      title: "Image 4",
+      link: "/videos/video-1",
+      description:"Nulla vitae elit libero, a pharetra augue mollis interdum."    
+    },
+    {
+      image: "assets/images/nature/5.jpg",
+      title: "Image 5",
+      link: "/videos/video-2",
+      description:"Nulla vitae elit libero, a pharetra augue mollis interdum."    
+    },
+    {
+      image: "assets/images/nature/6.png",
+      title: "Image 6",
+      link: "/videos/video-3",
+      description:"Nulla vitae elit libero, a pharetra augue mollis interdum."    
+    },
+  ];
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  preventNormal(event:any, image:any){
+    if (!image.prevented){
+      event.preventDefault();
+      //this.router.navigate(['./videos'])
+      this.router.navigate(['./videos'])
+    }
+   
   }
 
 }
